33bf192d9e2ba1d6bf261dbb1cc5847ea7202dfa
ryanmanns-ix <ryan.manns@imaginexconsulting.com>
2019-07-10 07:51:29 -0400

    Update double-commander.rb

--- Casks/double-commander.rb
+++ Casks/double-commander.rb
@@ -1,6 +1,6 @@
 cask 'double-commander' do
   version '0.9.3-8847'
-  sha256 'ecb8c9335ae178c5e1f29c0781315dc712e9567470d43c21ea6ffb517e0eb7da'
+  sha256 '6caa7dfdb28be70f26a4588d16b70df460220257a9a7f3260532e148eda6d65f'
 
   # downloads.sourceforge.net/doublecmd was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/doublecmd/doublecmd-#{version}.qt.x86_64.dmg"
